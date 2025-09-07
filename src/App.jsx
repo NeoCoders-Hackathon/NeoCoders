ce56e0a885578bbd0859d10d71c2f6059343040a
JASURBE77 <animesila89@gmail.com>
2025-09-07 11:52:12 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className='text-5xl  text-yellow-500'>
-    hello world
+   <div>
+    <Header />
    </div>
   )
 }
